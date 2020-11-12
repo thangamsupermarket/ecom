00e1bd9c9c6bb0057d3e86fa6794191777dcec76
thangamsupermarket <livi.livi.lesley@gmail.com>
2020-11-12 22:11:06 +0530

    Updates

---
+++ precache-manifest.72a4c06426765c8fcc7c18d75c0bbaca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82017e505d459849e710d12db144e378",
+    "url": "/ecom/index.html"
+  },
+  {
+    "revision": "5fc0063d265c32f7a70e",
+    "url": "/ecom/static/css/main.b6cdb482.chunk.css"
+  },
+  {
+    "revision": "998ee0dc4e34c0755008",
+    "url": "/ecom/static/js/2.d9b6b1d9.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/ecom/static/js/2.d9b6b1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc0063d265c32f7a70e",
+    "url": "/ecom/static/js/main.f5949a8a.chunk.js"
+  },
+  {
+    "revision": "eb01df3d1162ceac9d70",
+    "url": "/ecom/static/js/runtime-main.64758bbc.js"
+  }
+]);
